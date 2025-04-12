72cba165eda732337d1da3a0331bea2363dc897e
YesiBorges <yesibetborges@gmail.com>
2025-04-12 17:04:54 -0400

    Agregar Commit

---
+++ src/test/java/InstrumentoMusical.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.implementasonoro;
+
+/**
+ *
+ * @author HP
+ */
+public abstract class InstrumentoMusical implements Sonoro {
+    
+    public abstract void integranteOrquesta();
+    
+    
+    public void orquesta(){
+        
+        System.out.println("El instrumento suena en la orquesta");
+    }
+
+}
